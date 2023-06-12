57b9c714302632e6da7062630bf11c70eb6d55db
Bobahan <bobahansem98@icloud.com>
2023-06-12 21:39:12 +0300

    refactor redux/constants

---
+++ src/redux/constants/index.ts
@@ -0,0 +1,25 @@
+// Profile Reducer
+export const SET_USER_PROFILE = 'SET_USER_PROFILE';
+export const SET_ID = 'SET_ID';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_STATUS = 'SET_STATUS';
+export const UPDATE_STATUS = 'UPDATE_STATUS';
+export const SAVE_PHOTO = 'SAVE_PHOTO';
+
+// Users Reducer
+export const FOLLOW = 'FOLLOW';
+export const UNFOLLOW = 'UNFOLLOW';
+export const SET_USERS = 'SET_USERS';
+export const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+export const IS_FETCHING = 'IS_FETCHING';
+export const FOLLOWING_PROGRESS = 'FOLLOWING_PROGRESS';
+export const GET_USERS = 'GET_USERS';
+export const SET_FILTER = 'SET_FILTER';
+
+// Auth Reducer
+export const SET_USER = 'SET_USER';
+export const LOGOUT = 'LOGOUT';
+export const LOGIN = 'LOGIN';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const AUTH = 'AUTH';
